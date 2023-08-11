c137e5e5e6fe3727af7f6c22f367b77b378f3493
Henrique A Klein <henryaklein@gmail.com>
2023-08-11 10:29:23 -0300

    Fix: Base32.h broken during merge

--- src/Base32.h
+++ src/Base32.h
@@ -17,16 +17,8 @@ namespace TW::Base32 {
 /// Decode Base32 string, return bytes as Data
 /// alphabet: Optional alphabet, if missing, default ALPHABET_RFC4648
 inline bool decode(const std::string& encoded_in, Data& decoded_out, const char* alphabet_in = nullptr) {
-    size_t inLen = encoded_in.size();
-    // obtain output length first
-    size_t outLen = base32_decoded_length(inLen);
-#ifdef _MSC_VER
-    uint8_t *buf = (uint8_t *)_alloca(outLen);
-#else
-    uint8_t buf[outLen];
-#endif
-    if (alphabet_in == nullptr) {
-        alphabet_in = BASE32_ALPHABET_RFC4648;
+    if (encoded_in.empty()) {
+        return true;
     }
     Rust::CByteArrayResultWrapper res = Rust::decode_base32(encoded_in.c_str(), alphabet_in, false);
     if (res.isOk()) {
@@ -39,23 +31,10 @@ inline bool decode(const std::string& encoded_in, Data& decoded_out, const char*
 
 /// Encode bytes in Data to Base32 string
 /// alphabet: Optional alphabet, if missing, default ALPHABET_RFC4648
-inline std::string encode(const Data& val, const char* alphabet = nullptr) {
-    size_t inLen = val.size();
-    // obtain output length first, reserve for terminator
-    size_t outLen = base32_encoded_length(inLen) + 1;
-#ifdef _MSC_VER
-    char *buf = (char *)_alloca(outLen);
-#else
-    char buf[outLen];
-#endif
-    if (alphabet == nullptr) {
-        alphabet = BASE32_ALPHABET_RFC4648;
-    }
-    // perform the base32 encode
-    char* retval = base32_encode(val.data(), inLen, buf, outLen, alphabet);
-    if (retval == nullptr) {
-        // return empty string if failed
-        return std::string();
+inline std::string encode(const Data &val, const char *alphabet = nullptr, bool padding = false) {
+    auto res = Rust::encode_base32(val.data(), val.size(), alphabet, padding);
+    if (res.code != Rust::OK_CODE) {
+        return {};
     }
     std::string encoded_str(res.result);
     Rust::free_string(res.result);
