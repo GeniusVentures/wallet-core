697f238e4b4443fa17e095e37d085fb80c3dee4a
Henrique A Klein <henryaklein@gmail.com>
2023-11-14 14:57:52 -0300

    Fix: Visibility to default on windows static library

--- include/TrustWalletCore/TWData.h
+++ include/TrustWalletCore/TWData.h
@@ -23,51 +23,59 @@ typedef const void TWData;
 /// \param bytes Non-null raw bytes buffer
 /// \param size size of the buffer
 /// \return Non-null filled block of data.
-TWData *_Nonnull TWDataCreateWithBytes(const uint8_t *_Nonnull bytes, size_t size) TW_VISIBILITY_DEFAULT;
+TW_VISIBILITY_DEFAULT 
+TWData *_Nonnull TWDataCreateWithBytes(const uint8_t *_Nonnull bytes, size_t size);
 
 /// Creates an uninitialized block of data with the provided size.
 ///
 /// \param size size for the block of data
 /// \return Non-null uninitialized block of data with the provided size
-TWData *_Nonnull TWDataCreateWithSize(size_t size) TW_VISIBILITY_DEFAULT;
+TW_VISIBILITY_DEFAULT 
+TWData *_Nonnull TWDataCreateWithSize(size_t size);
 
 /// Creates a block of data by copying another block of data.
 ///
 /// \param data buffer that need to be copied
 /// \return Non-null filled block of data.
-TWData *_Nonnull TWDataCreateWithData(TWData *_Nonnull data) TW_VISIBILITY_DEFAULT;
+TW_VISIBILITY_DEFAULT 
+TWData *_Nonnull TWDataCreateWithData(TWData *_Nonnull data);
 
 /// Creates a block of data from a hexadecimal string.  Odd length is invalid (intended grouping to bytes is not obvious).
 ///
 /// \param hex input hex string
 /// \return Non-null filled block of data
-TWData *_Nullable TWDataCreateWithHexString(const TWString *_Nonnull hex) TW_VISIBILITY_DEFAULT;
+TW_VISIBILITY_DEFAULT 
+TWData *_Nullable TWDataCreateWithHexString(const TWString *_Nonnull hex);
 
 /// Returns the size in bytes.
 ///
 /// \param data A non-null valid block of data
 /// \return the size of the given block of data
-size_t TWDataSize(TWData *_Nonnull data) TW_VISIBILITY_DEFAULT;
+TW_VISIBILITY_DEFAULT 
+size_t TWDataSize(TWData *_Nonnull data);
 
 /// Returns the raw pointer to the contents of data.
 ///
 /// \param data A non-null valid block of data
 /// \return the raw pointer to the contents of data
-uint8_t *_Nonnull TWDataBytes(TWData *_Nonnull data) TW_VISIBILITY_DEFAULT;
+TW_VISIBILITY_DEFAULT 
+uint8_t *_Nonnull TWDataBytes(TWData *_Nonnull data);
 
 /// Returns the byte at the provided index.
 ///
 /// \param data A non-null valid block of data
 /// \param index index of the byte that we want to fetch - index need to be < TWDataSize(data)
 /// \return the byte at the provided index
-uint8_t TWDataGet(TWData *_Nonnull data, size_t index) TW_VISIBILITY_DEFAULT;
+TW_VISIBILITY_DEFAULT 
+uint8_t TWDataGet(TWData *_Nonnull data, size_t index);
 
 /// Sets the byte at the provided index.
 ///
 /// \param data A non-null valid block of data
 /// \param index index of the byte that we want to set - index need to be < TWDataSize(data)
 /// \param byte Given byte to be written in data
-void TWDataSet(TWData *_Nonnull data, size_t index, uint8_t byte) TW_VISIBILITY_DEFAULT;
+TW_VISIBILITY_DEFAULT 
+void TWDataSet(TWData *_Nonnull data, size_t index, uint8_t byte);
 
 /// Copies a range of bytes into the provided buffer.
 ///
@@ -75,7 +83,8 @@ void TWDataSet(TWData *_Nonnull data, size_t index, uint8_t byte) TW_VISIBILITY_
 /// \param start starting index of the range - index need to be < TWDataSize(data)
 /// \param size size of the range we want to copy - size need to be < TWDataSize(data) - start
 /// \param output The output buffer where we want to copy the data.
-void TWDataCopyBytes(TWData *_Nonnull data, size_t start, size_t size, uint8_t *_Nonnull output) TW_VISIBILITY_DEFAULT;
+TW_VISIBILITY_DEFAULT 
+void TWDataCopyBytes(TWData *_Nonnull data, size_t start, size_t size, uint8_t *_Nonnull output);
 
 /// Replaces a range of bytes with the contents of the provided buffer.
 ///
@@ -83,47 +92,55 @@ void TWDataCopyBytes(TWData *_Nonnull data, size_t start, size_t size, uint8_t *
 /// \param start starting index of the range - index need to be < TWDataSize(data)
 /// \param size size of the range we want to replace - size need to be < TWDataSize(data) - start
 /// \param bytes The buffer that will replace the range of data
-void TWDataReplaceBytes(TWData *_Nonnull data, size_t start, size_t size, const uint8_t *_Nonnull bytes) TW_VISIBILITY_DEFAULT;
+TW_VISIBILITY_DEFAULT 
+void TWDataReplaceBytes(TWData *_Nonnull data, size_t start, size_t size, const uint8_t *_Nonnull bytes);
 
 /// Appends data from a byte array.
 ///
 /// \param data A non-null valid block of data
 /// \param bytes Non-null byte array
 /// \param size The size of the byte array
-void TWDataAppendBytes(TWData *_Nonnull data, const uint8_t *_Nonnull bytes, size_t size) TW_VISIBILITY_DEFAULT;
+TW_VISIBILITY_DEFAULT 
+void TWDataAppendBytes(TWData *_Nonnull data, const uint8_t *_Nonnull bytes, size_t size);
 
 /// Appends a single byte.
 ///
 /// \param data A non-null valid block of data
 /// \param byte A single byte
-void TWDataAppendByte(TWData *_Nonnull data, uint8_t byte) TW_VISIBILITY_DEFAULT;
+TW_VISIBILITY_DEFAULT 
+void TWDataAppendByte(TWData *_Nonnull data, uint8_t byte);
 
 /// Appends a block of data.
 ///
 /// \param data A non-null valid block of data
 /// \param append A non-null valid block of data
-void TWDataAppendData(TWData *_Nonnull data, TWData *_Nonnull append) TW_VISIBILITY_DEFAULT;
+TW_VISIBILITY_DEFAULT 
+void TWDataAppendData(TWData *_Nonnull data, TWData *_Nonnull append);
 
 /// Reverse the bytes.
 ///
 /// \param data A non-null valid block of data
-void TWDataReverse(TWData *_Nonnull data) TW_VISIBILITY_DEFAULT;
+TW_VISIBILITY_DEFAULT 
+void TWDataReverse(TWData *_Nonnull data);
 
 /// Sets all bytes to the given value.
 ///
 /// \param data A non-null valid block of data
-void TWDataReset(TWData *_Nonnull data) TW_VISIBILITY_DEFAULT;
+TW_VISIBILITY_DEFAULT 
+void TWDataReset(TWData *_Nonnull data);
 
 /// Deletes a block of data created with a `TWDataCreate*` method.
 ///
 /// \param data A non-null valid block of data
-void TWDataDelete(TWData *_Nonnull data) TW_VISIBILITY_DEFAULT;
+TW_VISIBILITY_DEFAULT 
+void TWDataDelete(TWData *_Nonnull data);
 
 /// Determines whether two data blocks are equal.
 ///
 /// \param lhs left non null block of data to be compared
 /// \param rhs right non null block of data to be compared
 /// \return true if both block of data are equal, false otherwise
-bool TWDataEqual(TWData *_Nonnull lhs, TWData *_Nonnull rhs) TW_VISIBILITY_DEFAULT;
+TW_VISIBILITY_DEFAULT 
+bool TWDataEqual(TWData *_Nonnull lhs, TWData *_Nonnull rhs);
 
 TW_EXTERN_C_END
